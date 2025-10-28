d3be53d0f1e5c3492e581ca9489a433bc0716257
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-10-28 10:50:44 -0300

    chore(api/deps): configure fastify http framework, zod validation lib, cors, swagger and scalar for endpoints reference

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,42 @@
 import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhooks requests',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
 
 app.listen({ port: 3000, host: '0.0.0.0' }, () => {
-  console.log('🚀 Server is running on http://:3000/')
+  console.log('🔥 Server is running on http://localhost:3000/')
+  console.log('🔥 Documentation available at http://localhost:3000/docs')
 })
