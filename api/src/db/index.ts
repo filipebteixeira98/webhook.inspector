19a2866b03d6e9365c40a850be2026f511221a41
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-10-28 12:11:15 -0300

    chore(api): create webhooks schema

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -2,6 +2,9 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 
 import { env } from '@/env'
 
+import * as schema from '@/db/schema'
+
 export const db = drizzle(env.DATABASE_URL, {
+  schema,
   casing: 'snake_case',
 })
